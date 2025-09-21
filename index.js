3d8c56f8d0d187b5376ff96fa10541e6b96c3798
nirban07 <nirban012@gmail.com>
2025-09-21 23:48:54 +0530

    adds console log

--- index.js
+++ index.js
@@ -7,10 +7,15 @@ require('dotenv').config();
 const app = express();
 app.use(bodyParser.json());
 
+console.log('TENANT_ID:', process.env.TENANT_ID);
+console.log('CLIENT_ID:', process.env.CLIENT_ID);
+console.log('CLIENT_SECRET:', process.env.CLIENT_SECRET ? '✓ Loaded' : '✗ Missing');
+
+
 const lokka = spawn('cmd', ['/c', 'npx', '-y', '@merill/lokka'], {
   stdio: ['pipe', 'pipe', 'pipe'],
   env: {
-    ...process.env,
+    // ...process.env,
     TENANT_ID: process.env.TENANT_ID,
     CLIENT_ID: process.env.CLIENT_ID,
     CLIENT_SECRET: process.env.CLIENT_SECRET
